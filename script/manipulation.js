501e925ade0c8adad8605f43a02ab6250c966b06
Fadil939 <fadhlurrahman939@gmail.com>
2023-06-23 17:08:55 +0700

    classList

---
+++ script/manipulation.js
@@ -0,0 +1,21 @@
+const judul = document.getElementById("judul");
+judul.setAttribute("name", "fadhlur");
+//?untuk menambahkan atribut
+const a = document.querySelector("section#a a");
+a.setAttribute("id", "link");
+
+//? apa itu getAttribut untuk mengambil/mengetahui element
+//a.getAttribute("id");
+
+//?removeAtribut untuk mengapus atribut
+
+//?jika ingin mengelola class pake lah classList
+/*
+  element.classList.add()
+  element.classList.remove()
+  element.classList.toggle()
+  element.classList.item() =untuk mengetahui kelas tertentu dalam elemet
+  element.classList.contains() =cek apahkah sebuah element punya class tertentu
+  element.classList.replace()= mengangti class yang ada dengan class yang baru
+
+*/
